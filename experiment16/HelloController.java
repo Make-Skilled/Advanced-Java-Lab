0f5f3aafb474fce27e473fa525a96b285e12f38d
MS <72651078+maddydevgits@users.noreply.github.com>
2025-07-04 19:32:20 +0530

    Update HelloController.java

--- experiment16/HelloController.java
+++ experiment16/HelloController.java
@@ -2,15 +2,14 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/welcome")
-    public String display(@RequestParam("name") String name, Model model) {
-        model.addAttribute("msg", "Hello, " + name + "! Welcome to Spring MVC.");
-        return "welcome";
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("message", "Hello, " + name + "!");
+        return "hello";
     }
 }
